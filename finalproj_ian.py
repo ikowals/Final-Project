44a8419c8979e9cb8b45354d94a2fe82494ee258
ikowals <156926885+ikowals@users.noreply.github.com>
2024-04-22 23:40:34 -0400

    Proj w/o working enemy follow and wall reset

--- finalproj_ian.py
+++ finalproj_ian.py
@@ -70,38 +70,102 @@ class Wall(simpleGE.Sprite):
         self.position = (300,300)
         self.moveSpeed = .1
         self.colorRect("green",(50,50))
+
+    def reset(self):
+        self.x = 0
+        self.y = 0
+class Coin(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Orange.png")
+        self.setSize(25,25)
+        self.minSpeed= 3
+        self.maxSpeed = 8
+        self.minNegative = -3
+        self.maxNegative = -8
+        self.reset()
+        
+    def reset(self):
+        
+            self.y = random.randint(0, self.screenHeight)
+            self.x = random.randint(0, self.screenWidth)
+        
+            
+      
+    def checkBounds(self):
+        if self.bottom > self.screenHeight:
+            self.reset() 
+        elif self.y < 0:
+            self.reset()
+        elif self.x > 640:
+            self.reset()
+        elif self.x < 0:
+            self.reset()
+"""
     def process(self):
-        for enemy in self.scene.hurts:
-            dist = math.hypot(abs(self.x-enemy.x), abs(self.y-enemy.y))
-            if dist <= 100:
-                #print("please I need this")
-                self.timer = simpleGE.Timer()
-                self.timer.totalTime = 2
-                self.timer.start()
-                if self.timer == 10:
-                    print("clock checkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
+        for hurtz in self.scene.hurts:
+             if hurtz.collidesWith(self):
+                 print("b")
+                 self.x -= self.dx
+                 self.y -= self.dy
+                 self.speed = 0
+                 self.reset()
+                 #print("aaaaaaaaaaaa")
+"""            
+            
+            
                 
    
-    """
-    def process(self):
-        for enemy in self.scene.hurts:
-            if self.distanceTo((self.hurts.x,self.hurts.y)) >=1:
-                print("work")
-                self.timer = simpleGE.Timer()
-                self.timer.totalTime = 8
-                self.timer.start()
-                if time == 3:
-                   self.wall.color  = "blue"
-    """
+
     
 class Placer(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.colorRect("Blue", (50, 50))
-    """   
+        self.colorRect("Red", (20, 20))
     def process(self):
-        self.distanceTo(4,20) == place
-    """
+           keepGoing = True
+           self.correction = (0, 0)
+           if self.isKeyPressed(pygame.K_w):
+               """
+               if self.isKeyPressed(pygame.K_LEFT):
+                   self.moveAngle = 180
+                   self.speed += .1
+                   keepGoing = False
+               """
+               # come back to 145 degree movement if you have time. IDK why it wasn't working with walls earlier
+               self.moveAngle = 90
+               self.speed += .1
+               #self.speed += .1
+               keepGoing = False
+           elif self.isKeyPressed(pygame.K_s):
+               self.moveAngle = 270
+               self.speed += .1
+               keepGoing = False
+           elif self.isKeyPressed(pygame.K_a):
+               #self.moveAngle = 90
+               self.moveAngle = 180
+               self.speed += .1
+             
+               keepGoing = False
+           elif self.isKeyPressed(pygame.K_d):
+               #self.moveAngle -= 40
+               self.moveAngle = 0
+               self.speed += .1
+               keepGoing = False
+    
+           if self.speed > 5:
+               self.speed = 5
+           if keepGoing == True:
+               self.speed = 0
+           for barrier in self.scene.walls:
+                if self.isKeyPressed(pygame.K_q):
+                    print("a")
+                    barrier.x = self.x 
+                    barrier.y = self.y 
+                
+
+            
+    
 class Hurt(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -120,22 +184,22 @@ class Hurt(simpleGE.Sprite):
             #top
             self.y = 10
             self.x = random.randint(0, self.screenWidth)
-            self.dy = random.randint(self.minSpeed,self.maxSpeed)
+            #self.dy = random.randint(self.minSpeed,self.maxSpeed)
         if chooseSpot == int(2):
             #right
             self.y = random.randint(0, self.screenHeight)
             self.x = 640
-            self.dx = random.randint(self.maxNegative, self.minNegative)
+            #self.dx = random.randint(self.maxNegative, self.minNegative)
         if chooseSpot == int(3):
             #bottom
             self.y = 450
             self.x = random.randint(0, self.screenWidth)
-            self.dy = random.randint(self.maxNegative, self.minNegative)
+            #self.dy = random.randint(self.maxNegative, self.minNegative)
         if chooseSpot == int(4):
             #left, i' do this first cause  I knwo y = 0
             self.y = random.randint(0, self.screenHeight)
             self.x = 0
-            self.dx = random.randint(self.minSpeed,self.maxSpeed)
+            #self.dx = random.randint(self.minSpeed,self.maxSpeed)
         
     def checkBounds(self):
         if self.bottom > self.screenHeight:
@@ -155,11 +219,13 @@ class Hurt(simpleGE.Sprite):
                 self.x -= self.dx
                 self.y -= self.dy
                 self.speed = 0
+                self.reset()
                 #print("aaaaaaaaaaaa")
-        for barrier in self.scene.walls:
-            dist = math.hypot(abs(self.x-barrier.x), abs(self.y-barrier.y))
-            #if dist <= 100:
-                #print("bppp")
+    
+                
+                
+            
+                
 class LblScore(simpleGE.Label):
     def __init__(self):
         super().__init__()
@@ -173,17 +239,20 @@ class Game(simpleGE.Scene):
         self.sndHurt = simpleGE.Sound("hurtNoise.wav")
         self.knight = Knight(self)
         self.placer = Placer(self)
+        self.numCoins = 5
         #self.lblscore = LblScore()
         self.numHurt = 4
-        self.numWall = 1
+        self.numWall = 4
         self.walls = []
         self.hurts = []
+        self.coins = []
         for i in range(self.numWall):
             #newWall = Wall(self)
             #newWall.y= 300
             #newWall.x= (i*50)+ 125
             self.walls.append(Wall(self))
-        
+        for i in range(self.numCoins):
+            self.coins.append(Coin(self))
         for i in range(self.numHurt):
             self.hurts.append(Hurt(self))
      
@@ -191,7 +260,108 @@ class Game(simpleGE.Scene):
         self.sprites = [self.knight,
                         self.placer,
                         self.walls,
-                        self.hurts]
+                        self.hurts,
+                        self.coins]
+    def moveWall(self):
+        for hurtz in self.hurts:
+            if hurtz.collidesWith(self.walls):
+                self.walls.reset()
+        
+    def process(self):
+        #dist = math.hypot(abs(self.placer.x-self.knight.x), abs(self.placer.y-self.knight.y))
+        for coin in self.coins:
+            """
+            if self.charlie.collidesWith(self.coin):
+                self.sndCoin.play()
+                self.coin.reset()
+            """
+            if coin.collidesWith(self.knight):
+                coin.reset()
+                #self.sndCoin.play()
+                #self.score += 1
+                #self.lblscore.text = f"Score = {self.score}"
+        if self.placer.x > abs(self.knight.x + 40):
+            keepGoing1 = True
+            while keepGoing1:
+                self.placer.x -= 10
+                #self.placer.y -= 5
+                keepGoing1 = False
+        if self.placer.x < abs(self.knight.x - 40):
+            keepGoing2 = True
+            while keepGoing2:
+                #print("positive")
+                self.placer.x += 10
+                #self.placer.y += 5
+                keepGoing2 = False
+        if self.placer.y > abs(self.knight.y + 40):
+            keepGoing3 = True
+            while keepGoing3:
+                self.placer.y -= 10
+                #self.placer.y -= 5
+                keepGoing3 = False
+        if self.placer.y < abs(self.knight.y - 40):
+            keepGoing4 = True
+            while keepGoing4:
+                #print("positive")
+                self.placer.y += 10
+                #self.placer.y += 5
+                keepGoing4 = False
+    def follow(self):
+        if self.hurts.x > abs(self.knight.x+1):
+            keepGoing1 = True
+            while keepGoing1:
+                self.hurts.x -= 10
+                #self.placer.y -= 5
+                keepGoing1 = False
+        if self.hurts.x < abs(self.knight.x+1):
+            keepGoing2 = True
+            while keepGoing2:
+                #print("positive")
+                self.hurts.x += 10
+                #self.placer.y += 5
+                keepGoing2 = False
+        if self.hurts.y > abs(self.knight.y+1):
+            keepGoing3 = True
+            while keepGoing3:
+                self.hurts.y -= 10
+                #self.placer.y -= 5
+                keepGoing3 = False
+        if self.hurts.y < abs(self.knight.y+1):
+            keepGoing4 = True
+            while keepGoing4:
+                #print("positive")
+                self.hurts.y += 10
+                #self.placer.y += 5
+                keepGoing4 = False
+        
+        """    
+        else:
+            self.placer.x = self.placer.x
+            self.placer.y = self.placer.y
+        """
+            
+        #else:
+         #   keepGoing = False
+        
+        
+   
+        
+    """
+    def process(self):
+         for enemy in self.hurts:
+             dist = math.hypot(abs(self.x-enemy.x), abs(self.y-enemy.y))
+             if dist <= 100:
+                 #print("please I need this")
+                 
+                 self.timer = simpleGE.Timer()
+                 self.timer.totalTime = 20
+                 #self.timer.start()
+                 print(f"{self.Timer.getTimeLeft}")
+                 if self.timer.getTimeLeft() == 10:
+                     print("clock checkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
+                     self.stop()
+    """
+                 
     """
     def process(self):
         for hurt in self.hurts:
